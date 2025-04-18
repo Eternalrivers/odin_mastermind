f0cf10f944c32e188d96d629e9e179ef82b3f215
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-19 00:04:32 +0800

    feat: add method to generate computer color_code

--- lib/player/computer_player.rb
+++ lib/player/computer_player.rb
@@ -1,5 +1,16 @@
-# Computer player that inherits from class Player
-
+# Inherits from class Player
 require_relative '../player'
+# Computer player that inherits from class Player
 class ComputerPlayer < Player
+  COLOR = %w[BLUE ORANGE GREEN VIOLET].freeze
+  def random_color
+    c = rand(0..3)
+    COLOR[c]
+  end
+
+  def create_code
+    (0..3).each do |color|
+      @color_code[color] = random_color
+    end
+  end
 end
