ea23e51abe0d38e3072613aee7b719c8c4d54ddf
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-19 03:13:07 +0800

    feat: add method for human player input

--- lib/player/human_player.rb
+++ lib/player/human_player.rb
@@ -1,5 +1,18 @@
-# Human player that inherits from class Player
+# that inherits from class Player
 
 require_relative '../player'
+# Human player
 class HumanPlayer < Player
+  COLOR = %w[BLUE ORANGE GREEN VIOLET].freeze
+
+  def input_color_code
+    puts 'BLUE, ORANGE, GREEN, VIOLET'
+    i = 0
+    while i < 4
+      puts "Please guess the No.#{i + 1} color  "
+      @color_code[i] = gets.upcase.chomp
+      i += 1
+    end
+    puts "You've chosen #{@color_code}"
+  end
 end
