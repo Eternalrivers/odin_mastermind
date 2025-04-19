e54be78165f42df7d5fe8a2a5d801629ac365fb0
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-20 04:19:49 +0800

    fix: prevent LoadError
    Wrong relative path for the class required.

--- main.rb
+++ main.rb
@@ -1,6 +1,3 @@
-require_relative 'lib/player/computer_player'
-require_relative 'lib/player/human_player'
+require_relative 'lib/game'
 
-human_player = HumanPlayer.new('Player 1')
-
-puts human_player.name
+Game.new(HumanPlayer, ComputerPlayer).play
