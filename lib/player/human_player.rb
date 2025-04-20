83d61f595f8882d6fc5816a137e1e127f3e66217
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-20 15:33:45 +0800

    feat: move input to a new method

--- lib/player/human_player.rb
+++ lib/player/human_player.rb
@@ -3,14 +3,22 @@ require_relative '../player'
 class HumanPlayer < Player
   COLOR = %w[BLUE ORANGE GREEN VIOLET].freeze
 
-  def input_color_code
-    puts 'BLUE, ORANGE, GREEN, VIOLET'
+  def input
     i = 0
     while i < 4
       puts "\nPlease guess the No.#{i + 1} color  "
       @color_code[i] = gets.upcase.chomp
+      unless COLOR.include?(@color_code[i])
+        puts "Wrong Input! Please choose one of the colors #{COLOR}"
+        redo
+      end
       i += 1
     end
+  end
+
+  def input_color_code
+    puts 'BLUE, ORANGE, GREEN, VIOLET'
+    input
     puts "You've chosen #{@color_code}"
   end
 end
