5611e0aa809c15054816da09be1d8a65353da4fe
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-28 16:03:34 +0800

    feat: add feedback methods to game.rb
    Add feedback methods from the computer player to game for encapsulation

--- lib/game.rb
+++ lib/game.rb
@@ -19,4 +19,46 @@ class Game
       break if i == 12 || @human_player.color_code == @computer_player.color_code
     end
   end
+
+  def white_feedback
+    @color_code.reduce([]) { |color_code, color| color_code << color }
+  end
+
+  def red_feedback(player)
+    player.color_code.reduce([]) { |color_code, color| color_code << color }
+  end
+
+  def code_white(player)
+    @white_feedback = white_feedback
+    @red_feedback = red_feedback(player)
+    @red_feedback.each_with_index do |color, index|
+      next unless @white_feedback[index] == (color)
+
+      @color_feedback << 'WHITE'
+      @white_feedback[index] = nil
+      @red_feedback[index] = nil
+    end
+  end
+
+  def code_red
+    @red_feedback.each do |color|
+      next if color.nil?
+      next unless @white_feedback.include?(color)
+
+      index = @white_feedback.find_index(color)
+      @color_feedback << 'RED'
+      @white_feedback[index] = nil
+    end
+  end
+
+  def reset
+    @color_feedback = []
+  end
+
+  def create_feedback(player)
+    code_white(player)
+    code_red
+    puts @color_feedback
+    reset
+  end
 end
