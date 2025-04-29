5e65871ebfed408bcfcf88a1b93365c31ef3f81e
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-29 09:48:33 +0800

    feat: add method to select as code maker/breaker

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
 require_relative 'lib/game'
 
-Game.new(HumanPlayer, ComputerPlayer).play
+puts 'Do you want to be the code maker or the code breaker?'
+loop do
+  @code = gets.chomp.upcase
+  break if @code == 'CODE MAKER' || @code == 'CODE BREAKER'
+
+  puts 'Invalid input. Please choose between CODE MAKER or CODE BREAKER'
+end
+
+if @code == 'CODE MAKER'
+  code_maker = HumanPlayer
+  code_breaker = ComputerPlayer
+else
+  code_maker = ComputerPlayer
+  code_breaker = HumanPlayer
+end
+
+Game.new(code_breaker, code_maker).play
